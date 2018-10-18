f778e7c355b1e7503774883a9ad7cbb1c36f9b39
allana <allana@thehangar.cr>
2018-10-18 09:57:15 -0600

    k

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,6 +1,23 @@
 $( document ).ready(function() {
 
   // Get started!
-  alert('YES');
+  // alert('YES');
+
+  // function myfunction(X) {
+  //   console.log('entra');
+  //   X.classList.toggle("change");
+  // }
+
+
+  var burguer = document.getElementsByClassName('navbar-toggler');
+
+  if (burguer.length > 0) {
+
+    burguer[0].onclick = function() {  
+      burguer[0].classList.toggle("change");
+    };
+
+  };
+
 
 });
